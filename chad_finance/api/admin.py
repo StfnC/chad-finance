510cbdbcb41248f6a3f7e3038f0e783e4f070ccf
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-02-15 17:02:37 -0500

    personnalized admin

--- chad_finance/api/admin.py
+++ chad_finance/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = "Chad Finance"
+admin.site.site_title = "Chad Finance"
+admin.site.index_title = "Chad Finance"
