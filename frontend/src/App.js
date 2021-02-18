bd3231493c7da45cb1a35e103869778956ffd735
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-02-18 18:42:30 -0500

    ajouter jf et goodbye marc !

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,12 @@ import HomePage from './components/HomePage';
 
 function App() {
   return (
+    <div> 
       <HomePage name="Stefan" />
+      <HomePage name="JF" />
+      <HomePage name="GoodBye Marc" />
+
+      </div>
   );
 }
 
