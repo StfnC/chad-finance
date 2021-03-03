b0fe2eacc03909d18cb8db36b15b8bbe8af4e3d9
Jean-François Senécal <jeanjeansenecal@gmail.com>
2021-03-02 22:42:13 -0500

    Creation de la serach bar dans home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="center"> 
       <Login />
+      <HomePage />
 
       </div>
   );
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component, useState} from 'react';
 import { render } from 'react-dom';
-import { Grid, Button, ButtonGroup, Typography, TextField, Paper } from "@material-ui/core";
+import { Grid, Button, Typography, TextField, Paper, Container, makeStyles} from "@material-ui/core";
+import { lightBlue, lightGreen } from '@material-ui/core/colors';
 /*import {
   BrowserRouter as Router,
   Switch,
@@ -9,39 +10,74 @@ import { Grid, Button, ButtonGroup, Typography, TextField, Paper } from "@materi
   Redirect,
 } from "react-router-dom"; */
 
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        "& > *": {
+       // width: theme.spacing(60),
+       // height: theme.spacing(20),
+        backgroundColor: lightGreen[50],
+        },
+       
+    },
+    space:{
+        margin: theme.spacing(1),
+    }
+    
+
+}));
+
+
  export default function Login (props) {
     const {classes} = props;
         const [email, setEmail] = useState('');
         const [password, setPassword] = useState('');
+        const classe = useStyles();
     
        function handleSubmit(event) {
             event.preventDefault();
             console.log( 'Email:', email, 'Password: ', password); 
-           // You should see email and password in console.
-           // ..code to submit form to backend here...
-    
+           
+           /* fetch("/api/")
+            .then((response) => response.json())
+            .then((data) => 
+            console.log(data)
+            ); */
+     
         }
+
     
-        return( <div >
-                <Paper>
+        return( <div className={classe.root}>
+          
+                <Paper >
+                    <Typography className={classe.space} align= "center" color="textPrimary" variant ="h2">Log in</Typography>
                     <form onSubmit={handleSubmit} >
                         <TextField
-                                              
-                            value={email}
-                            onInput={ e=>setEmail(e.target.value)}
+                           className={classe.space}
+                           label="Username"
+                           variant ="outlined"      
+                           placeholder="nom d'utilisateur"             
+                           value={email}
+                           onInput={ e=>setEmail(e.target.value)}
+                           
                             
     
                         />
                         <TextField
-                            
+                            className={classe.space}
+                            label = "Mot de passe"
+                            variant = "outlined"
+                            placeholder = "Mot de passe"
                             value={password}
                             onInput={ e=>setPassword(e.target.value)}
                             
                         />
                         <Typography />
                         <Button
-                            type="submit"                            
-                            
+                            className={classe.space}
+                            type="submit"    
+                            variant="contained" 
+                            color="primary"                                                  
                         >
                             Login
                         </Button>
@@ -50,6 +86,7 @@ import { Grid, Button, ButtonGroup, Typography, TextField, Paper } from "@materi
                         Pas encore de compte? <a href ="http://localhost:8000/admin">Regsiter</a>
                     </p>
                 </Paper>
+                
             </div>
         );
 }
