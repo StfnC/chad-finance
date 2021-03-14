64cb5525b4f146f55d1ca4b5da040a02e948b6b0
Alex_Des <alexis.desforges@icloud.com>
2021-03-13 20:29:00 -0500

    Views et serializers

--- chad_finance/api/views.py
+++ chad_finance/api/views.py
@@ -1,3 +1,38 @@
+from .models import UserAccount, Portfolio, Trade
+from .serializers import PortfolioSerializer, TradeSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+# portfolio views 
+
+class PortfolioRetrieveAPIView(generics.RetrieveAPIView):
+	queryset = Portfolio.objects.all()
+	serializer_class = PortfolioSerializer
+
+
+class PorfolioListAPIView(generics.ListAPIView):
+	queryset = Portfolio.objects.all()
+	serializer_class = PortfolioSerializer
+
+
+class PortfolioListCreate(generics.ListCreateAPIView):
+   queryset = Portfolio.objects.all()
+	serializer_class = PortfolioSerializer
+
+
+# trade views
+
+class TradeRetrieveAPIView(generics.RetrieveAPIView):
+	queryset = Trade.objects.all()
+	serializer_class = TradeSerializer
+
+
+class TradeListAPIView(generics.ListAPIView):
+	queryset = Trade.objects.all()
+	serializer_class = TradeSerializer
+
+
+class TradeListCreate(generics.ListCreateAPIView):
+  queryset = Trade.objects.all()
+	serializer_class = TradeSerializer
