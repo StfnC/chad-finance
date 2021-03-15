04f4aa675317f4c553c75fd49c4868f2a9940a00
Alex_Des <alexis.desforges@icloud.com>
2021-03-15 15:22:55 -0400

    Views/urls remodeling 2.0

--- chad_finance/api/urls.py
+++ chad_finance/api/urls.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import PortfolioCreateAPIView, PortfolioRetrieveAPIView, TradeCreateAPIView, TradeListAPIView, TradeRetrieveAPIView
 
 urlpatterns = [
 
     # portfolio views
 
-    path('portfolio/<int:pk>', views.PortfolioRetrieveUpdateDestroyAPIView.as_view()),
-    path('portfolio/', views.PortfolioCreateAPIView.as_view()),
+    path('portfolio/<int:pk>', PortfolioRetrieveAPIView.as_view()),
+    path('portfolio/', PortfolioCreateAPIView.as_view()),
 
-    # trade views 
+    # trade views
 
-    path('trade/<int:pk>', views.TradeRetrieveUpdateDestroyAPIView.as_view()),
-    path('trade/', views.TradeCreateAPIView.as_view())
-    path('trade/all', views.TradeListAPIView.as_view())
+    path('trade/<int:pk>', TradeRetrieveAPIView.as_view()),
+    path('trade/', TradeCreateAPIView.as_view()),
+    path('trade/all', TradeListAPIView.as_view()),
 ]
+
+# quand un l'utilisateur est dirige vers un url la 'view' appropriee est appelee automatiquement
--- chad_finance/api/views.py
+++ chad_finance/api/views.py
@@ -4,26 +4,35 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# portfolio views 
 
-class PortfolioRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):   
+
+# portfolio views
+
+
+class PortfolioRetrieveAPIView(generics.RetrieveUpdateDestroyAPIView):
+	"""
+    Vue qui permet de récuperer l'information sur un portfolio
+    """
 	queryset = Portfolio.objects.all()
 	serializer_class = PortfolioSerializer
 
 
-class PortfolioCreateAPIView(generics.CreateAPIView):    
+class PortfolioCreateAPIView(generics.CreateAPIView):
 	queryset = Portfolio.objects.all()
 	serializer_class = PortfolioSerializer
 
 
 # trade views
-
-class TradeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView): 
+	
+class TradeRetrieveAPIView(generics.RetrieveUpdateDestroyAPIView):
+	"""
+    Vue qui permet de récuperer l'information sur un trade
+    """
 	queryset = Trade.objects.all()
 	serializer_class = TradeSerializer
 
 
-class TradeCreateAPIView(generics.CreateAPIView):      
+class TradeCreateAPIView(generics.CreateAPIView):
 	queryset = Trade.objects.all()
 	serializer_class = TradeSerializer
 
@@ -31,3 +40,4 @@ class TradeCreateAPIView(generics.CreateAPIView):
 class TradeListAPIView(generics.ListAPIView):
 	queryset = Trade.objects.all()
 	serializer_class = TradeSerializer
+
