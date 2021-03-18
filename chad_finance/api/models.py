00c2f4ffdca95fef685276bdca77859a72d80b02
Marc <aarajmarc@gmail.com>
2021-03-17 20:37:13 -0400

    Added AlphaVantage key reference in settings.py Made Portfolio and Trade models Created method get_daily_price in Trade Created method calculate_total_value in Portfolio

--- chad_finance/api/models.py
+++ chad_finance/api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserAccountManager
+from django.conf import settings
+from datetime import date
+from alpha_vantage.timeseries import TimeSeries
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
@@ -24,14 +27,6 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
-class Portfolio(models.Model):
-    owner = models.OneToOneField(to="UserAccount", on_delete=models.CASCADE)
-    creation_date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Portfolio of {self.owner}"
-
-
 class Trade(models.Model):
     portfolio = models.ForeignKey(to="Portfolio", on_delete=models.CASCADE)
     symbol = models.CharField(max_length=5, blank=False)
@@ -39,5 +34,34 @@ class Trade(models.Model):
     buy_date = models.DateTimeField(auto_now_add=True)
     quantity = models.DecimalField(max_digits=50, decimal_places=2, blank=False)
 
+    def get_daily_price(self):
+        API_KEY = str(settings.ALPHA_VANTAGE_KEY)
+        ts = TimeSeries(key=API_KEY)
+        data, metadata = ts.get_daily_adjusted(symbol=self.symbol, outputsize="compact")
+        return data
+
     def __str__(self):
         return f"Symbol: {self.symbol} from {self.portfolio}"
+
+
+class Portfolio(models.Model):
+    DEFAULT_AMOUNT = 200000
+
+    owner = models.OneToOneField(to="UserAccount", on_delete=models.CASCADE)
+    creation_date = models.DateField(auto_now_add=True)
+    starting_amount = models.DecimalField(max_digits=9, decimal_places=2, default=DEFAULT_AMOUNT)
+    current_amount = models.DecimalField(max_digits=9, decimal_places=2, default=DEFAULT_AMOUNT)
+
+    def calculate_total_value(self):
+        trades = self.trade_set.all()
+        total_value = 0.0
+        for trade in trades:
+            data = trade.get_daily_price()
+            total_value = total_value + float(data[str(date.today())]['4. close']) * float(trade.quantity)
+        return total_value
+
+    def modify_amount(self, new_value):
+        self.current_amount = new_value
+
+    def __str__(self):
+        return f"Portfolio of {self.owner}"
