972414c2d9a34070c2f777fa05a87f9707cee77a
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2021-04-12 14:09:52 -0400

    Fixed symbol name not updating

--- frontend/src/components/SymbolDetails.js
+++ frontend/src/components/SymbolDetails.js
@@ -2,15 +2,13 @@ import { useState, useRef, useEffect } from "react";
 import { createChart } from "lightweight-charts";
 import Chart from "./Chart";
 
-const minValue = 1;
-const maxValue = 99999999999999999999999999999999999999999999999999;
-
 const SymbolDetails = ({ match }) => {
-
     const [chartData, setChartData] = useState([]);
-    
-    const [SymbolOverview, setSymbolOverview] = useState({}); 
-    
+
+    const [symbolOverview, setSymbolOverview] = useState([
+        { Name: "", Symbol: "" },
+    ]);
+
     const initSymbolInfo = async () => {
         // Permet de recuperer les donnees pour construire le graphique
         try {
@@ -30,7 +28,7 @@ const SymbolDetails = ({ match }) => {
             // On retourne la reponse sous format JSON
             const res_json = JSON.parse(await res.json());
             setChartData(res_json.chart_data);
-            setSymbolOverview(res_json.info);
+            setSymbolOverview(res_json.info[0]);
         } catch (error) {
             console.log(error);
         }
@@ -38,20 +36,18 @@ const SymbolDetails = ({ match }) => {
 
     useEffect(() => {
         initSymbolInfo();
-    }, []);
+    });
 
     return (
         <div>
-            <h1>{SymbolOverview[0]["Name"]} ({SymbolOverview[0]["Symbol"]})</h1>
-            <p>Description</p>
+            <h1>
+                {symbolOverview["Name"]} ({symbolOverview["Symbol"]})
+            </h1>
             <Chart
                 key={chartData.toString()}
                 data={chartData}
                 chartType="candle"
             />
-            <input type="number" min={minValue} max={maxValue}></input>
-            <button>Buy</button>
-            <button>Sell</button>
             {/* Company Overview */}
         </div>
     );
